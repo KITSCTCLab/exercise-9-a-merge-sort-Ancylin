8c98fa436b97c63f8acfacfafdf974c9c4fa02bb
Ancylin <100009469+Ancylin@users.noreply.github.com>
2022-11-04 22:52:05 +0530

    first commit

--- Main.py
+++ Main.py
@@ -1,16 +1,44 @@
 from typing import List
 
-def merge_sort(data) -> None:
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   # Write code here
+  array = [0 for i in range(m + n)]
+  
+  i = 0
+  j = 0
+  k = 0
+  
+  while i < m and j < n:
+    if nums1[i] < nums2[j]:
+      array[k] = nums1[i]
+      i += 1
+    else:
+      array[k] = nums2[j]
+      j += 1
+    k += 1
+  
+  while i < m:
+    array[k] = nums1[i]
+    i += 1
+    k += 1
+  
+  while j < n:
+    array[k] = nums2[j]
+    j += 1
+    k += 1
+  
+  for i in range(m + n):
+    nums1[i] = array[i]
 
 
 # Do not change the following code
-input_data = input()
-data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-merge_sort(data)
-print(data)
+nums1 = []
+nums2 = []
+for item in input().split(', '):
+  nums1.append(int(item))
+for item in input().split(', '):
+  nums2.append(int(item))
+m = int(input())
+n = int(input())
+merge(nums1, m, nums2, n)
+print(nums1)
